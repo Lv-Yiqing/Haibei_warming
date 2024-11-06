554363ca30013a1c1deaef90145e196562a87586
Lv-Yiqing <lvyq@stu.pku.edu.cn>
2024-11-06 19:10:24 +0800

    Update treatment effects on viral diversity by LMM.R

--- treatment effects on viral diversity by LMM.R
+++ treatment effects on viral diversity by LMM.R
@@ -1,8 +1,8 @@
-##0. read resampled votu dataset
+#0. read resampled votu dataset
 otu <- read.csv("resampled_votu.csv",row.names = 1,header=T)
 otu <- as.data.frame(t(otu))
 
-##1. calculate alpha diversity
+#1. calculate alpha diversity
 library(vegan)
 shannon <- diversity(otu,"shannon")
 simpson <- diversity(otu,"simpson")
@@ -16,15 +16,13 @@ diversity$treatment <- rep(c(rep("0",4),rep("1",4)),4)
 diversity$depth <- rep(c(0.05,0.20,0.45,0.80),8)
 diversity$block <- c(rep(1,8),rep(2,8),rep(3,8),rep(4,8))
 
-##2. treatment effects by linear mixed model
+#2. treatment effects by linear mixed model
 library(lme4)
 library(lmerTest)
 
 m1 <- lmer(shannon ~ treatment*depth+(1 |block),data = diversity)
 summary(m1)
-
 m2 <- lmer(simpson ~ treatment*depth+(1 |block),data = diversity)
 summary(m2)
-
 m3 <- lmer(Pielou ~ treatment*depth+(1 |block),data = diversity)
 summary(m3)
