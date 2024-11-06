cceb8815c99e4930a98832244de2b22772be4610
Lv-Yiqing <lvyq@stu.pku.edu.cn>
2024-11-06 19:09:17 +0800

    Update MRM-VPA.R

--- MRM-VPA.R
+++ MRM-VPA.R
@@ -2,29 +2,29 @@ library(ecodist)
 library(vegan)
 
 #1. read environmental variables data and calculate distance matrix
-env=read.csv("env.csv",header = T,row.names = 1)
-pHdis<-vegdist(scale(env$pH),method="euclidean")
-SWCdis<-vegdist(scale(env$SWC),method="euclidean")
-SOCdis<-vegdist(scale(env$SOC.g.kg.1),method="euclidean")
-ANdis<-vegdist(scale(env$AN.mg.kg.1),method="euclidean")
-MBCdis<-vegdist(scale(env$MBC.mg.kg.1),method="euclidean")
-MATdis<-vegdist(scale(env$MAT..),method="euclidean")
+env <- read.csv("env.csv",header = T,row.names = 1)
+pHdis <- vegdist(scale(env$pH),method="euclidean")
+SWCdis <- vegdist(scale(env$SWC),method="euclidean")
+SOCdis <- vegdist(scale(env$SOC.g.kg.1),method="euclidean")
+ANdis <- vegdist(scale(env$AN.mg.kg.1),method="euclidean")
+MBCdis <- vegdist(scale(env$MBC.mg.kg.1),method="euclidean")
+MATdis <- vegdist(scale(env$MAT..),method="euclidean")
 
 #2. read host community data (resample_motu) and calculate distance matrix
-host=read.csv("resampled_motu.csv",header = T, row.names = 1)
-host=as.data.frame(t(host))
-hostdis=vegdist(host,method="bray")
+host <- read.csv("resampled_motu.csv",header = T, row.names = 1)
+host <- as.data.frame(t(host))
+hostdis <- vegdist(host,method="bray")
 
 #3. read viral community data (resample_votu) and calculate distance matrix
-votu=read.csv("resampled_votu.csv",header = T,row.names = 1)
-votu=as.data.frame(t(votu))
-votudis=vegdist(votu,method="bray")
+votu <- read.csv("resampled_votu.csv",header = T,row.names = 1)
+votu <- as.data.frame(t(votu))
+votudis <- vegdist(votu,method="bray")
 
 #4. read geographic distance data and calculate distance matrix
-geo=read.csv("geodis.csv",header = T,row.names = 1)
-geodis=vegdist(geo,method="euclidean")
-min.dist=15
-lngeodis=log(geodis+min.dist) #log transform
+geo <- read.csv("geodis.csv",header = T,row.names = 1)
+geodis <- vegdist(geo,method="euclidean")
+min.dist <- 15
+lngeodis <- log(geodis+min.dist) #log transform
 
 
 #5.MRM-VPA analysis
