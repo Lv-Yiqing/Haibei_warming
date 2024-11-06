25674080977db03e1f54fa609618c6d3def23cc1
Lv-Yiqing <lvyq@stu.pku.edu.cn>
2024-11-06 19:15:54 +0800

    Update PCoA and PERMANOVA.R

--- PCoA and PERMANOVA.R
+++ PCoA and PERMANOVA.R
@@ -12,7 +12,7 @@ data_pcoa <- cmdscale(data_dist,k=(nrow(otu)-1),eig=TRUE)
 sample_site <- data.frame(data_pcoa$points)[1:2] 
 sample_site$treatment <- rep(c("control","control","control","control","warming","warming","warming","warming"),4)
 sample_site$depth <- rep(c("0-10","10-30","30-60","60-100"),8)
-names(sample_site)[1:5] <- c('PCoA1', 'PCoA2','treatment',"depth")
+names(sample_site)[1:4] <- c('PCoA1', 'PCoA2','treatment',"depth")
 
 #3. PERMANOVA
 result <- adonis2(formula = otu ~ (sample_site$treatment * sample_site$depth),data =sample_site, permutations = 999, method = "bray") 
